afdadc975fc74f1af64b53f19a79e2b87b5b23bf
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-02 16:30:33 -0600

    Create stats.hpp

---
+++ incremental-statistics/c++/stats.hpp
@@ -0,0 +1,39 @@
+#include <cmath>
+#include <iostream>
+#include <set>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+// Implement functions to track the max, min, mean, and mode
+
+class TempTracker
+{
+public:
+    void insert(int temperature)
+    {
+
+    }
+
+    int getMax() const
+    {
+        return 0;
+    }
+
+    int getMin() const
+    {
+        return 0;
+    }
+
+    double getMean() const
+    {
+        return 0.0;
+    }
+
+    int getMode() const
+    {
+        return 0;
+    }
+};
