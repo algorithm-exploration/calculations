45596610cfb256b42117389b75b75179ddee6152
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-02 16:29:58 -0600

    Create stats.h

---
+++ incremental-statistics/c-lang/stats.h
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+// implement functions to track the max, min, mean, and mode
+
+
+typedef struct {
+
+    // add necessary data here
+
+} TempTracker;
+
+// initializes temperature tracker
+void initializeTracker(TempTracker *tracker)
+{
+
+}
+
+// records a new temperature
+void insertTemperature(TempTracker *tracker, int temperature)
+{
+
+}
+
+// returns the highest temp we've seen so far
+int getMax(TempTracker *tracker)
+{
+    return 0;
+}
+
+// returns the lowest temp we've seen so far
+int getMin(TempTracker *tracker)
+{
+    return 0;
+}
+
+// returns the mean of all temps we've seen so far
+double getMean(TempTracker *tracker)
+{
+    return 0.0;
+}
+
+// return a mode of all temps we've seen so far
+int getMode(TempTracker *tracker)
+{
+    return 0;
+}
