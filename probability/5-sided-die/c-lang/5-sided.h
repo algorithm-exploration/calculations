7b5f27518c9a849314e7a02a16d0f0e70a99844c
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:41:52 -0600

    Create 5-sided.h

---
+++ probability/5-sided-die/c-lang/5-sided.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rand7()
+{
+    return ((rand() / (1.0 + RAND_MAX)) * 7) + 1;
+}
+
+int rand5()
+{
+    // implement rand5() using rand7()
+    
+
+    return 0;
+}
