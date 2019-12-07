458c50072815d62ced715fb8144650da0c1a55b1
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:41:02 -0600

    Create 7-sided.h

---
+++ probability/7-sided-die/c-lang/7-sided.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rand5(void)
+{
+    return ((rand() / (1.0 + RAND_MAX)) * 5) + 1;
+}
+
+int rand7(void)
+{
+    // implement rand7() using rand5()
+    
+
+    return 0;
+}
