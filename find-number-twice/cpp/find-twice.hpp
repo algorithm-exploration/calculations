6f7f531891d48eafed37201de425fa3f4713e6ab
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:28:26 -0600

    Create find-twice.hpp

---
+++ find-number-twice/cpp/find-twice.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+unsigned int findRepeat(const vector<unsigned int>& numbers)
+{
+    // find the number that appears twice
+    
+
+    return 0;
+}
