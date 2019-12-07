90873cca849773ace008fcc714f99ec129a9178e
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:42:51 -0600

    Create find-twice.h

---
+++ find-number-twice/c-lang/find-twice.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+unsigned int findRepeat(const unsigned int* numbers, size_t size)
+{
+    // find the number that appears twice
+    
+
+    return 0;
+}
